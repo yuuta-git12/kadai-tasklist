8e8176f7cf93e46a5bbe772003660c7572f0a3da
yuuta.git12 <tayu2201@icloud.com>
2018-09-12 14:41:19 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
+  #root to: #'tasks#index'
   resources :tasks
+  
+  root to: 'toppages#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
+
 end
